b67c9b903dc5acb88f33ce2c7e311aee050d457f
wahab95 <a.wahab1996@gmail.com>
2024-06-04 21:33:55 +0300

    chore: Add HTML structure and JavaScript function for game logic

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+function getComputerChoice(){
+    let rand = Math.random()*3;
+    rand = Math.round(rand)
+    if(rand === 0 ){
+        return "Rock";
+    }
+    else if(rand === 1){
+        return "Paper";
+    }
+    else{
+        return "Scissors"
+    }
+}
