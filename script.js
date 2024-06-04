2ce6bb0cc1663fa851e19edb92e15697aadf3710
wahab95 <a.wahab1996@gmail.com>
2024-06-04 21:48:09 +0300

    chore: Refactor getComputerChoice function to return lowercase choices

--- script.js
+++ script.js
@@ -2,12 +2,35 @@ function getComputerChoice(){
     let rand = Math.random()*3;
     rand = Math.round(rand)
     if(rand === 0 ){
-        return "Rock";
+        return "rock";
     }
     else if(rand === 1){
-        return "Paper";
+        return "paper";
     }
     else{
-        return "Scissors"
+        return "scissors"
+    }
+}
+function getHumanChoice(h = prompt('Choose your Choice')){
+    return h.toLowerCase();
+}
+let humanCounter = 0;
+let computerCounter = 0;
+
+function playRound(human, computer){
+    if(human === 'rock' && computer == 'scissors'){
+        humanCounter++;
+    }
+    else if(human === 'scissors' && computer == 'paper'){
+        humanCounter++;
+    }
+    else if(human === 'paper' && computer == 'rock'){
+        humanCounter++;
+    }
+    else if(human === computer){
+        
+    }
+    else{
+        computerCounter++;
     }
 }
